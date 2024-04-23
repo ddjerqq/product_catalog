7199e80af512dc44aa7a711bbdbad087b690ceff
Coding <d.gogodze@homtail.com>
2024-04-23 16:24:10 +0400

    Added Smart Ctr

--- src/Domain/Entities/Item.cs
+++ src/Domain/Entities/Item.cs
@@ -2,11 +2,19 @@
 
 public class Item
 {
+    public static Item NewItem(string name, User? owner) => new Item
+    {
+        Id = Guid.NewGuid(),
+        Name = name,
+        Rarity = Random.Shared.NextSingle(),
+        Owner = owner
+     };
+
     public Guid Id { get; set; }
 
     public string Name { get; set; } = default!;
 
     public float Rarity { get; set; }
 
-    public User Owner { get; set; } = default!;
+    public User? Owner { get; set; } = default!;
 }
