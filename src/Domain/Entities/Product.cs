5554114f795a2ce5cc1de08308a464a212995a89
10x developer <gio2004n@gmail.com>
2024-04-23 14:40:38 +0400

    initial commit

---
+++ src/Domain/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities;
+
+public class Product
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = default!;
+
+    public decimal Price { get; set; }
+
+    public int Quantity { get; set; }
+}
