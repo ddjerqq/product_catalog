e0cfad17c8659ffc38c87ee1672698b493090f28
10x developer <gio2004n@gmail.com>
2024-04-25 13:44:57 +0400

    added persistence

---
+++ src/Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common.Abstractions;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options), IDbContext
+{
+    public DbSet<User> Users => Set<User>();
+
+    public DbSet<Item> Item => Set<Item>();
+}
---
+++ src/Infrastructure/WebHostBuilderExt.cs
@@ -0,0 +1,43 @@
+﻿using Application.Common.Abstractions;
+using Infrastructure.Persistence;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class WebHostBuilderExt
+{
+    public static void AddUtcDateTimeProvider(this WebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.AddScoped<IDateTimeProvider, UtcDateTimeProvider>();
+        });
+    }
+
+    public static void AddEmailProviders(this WebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.AddScoped<IEmailSender, GoogleEmailSender>();
+            services.AddScoped<IEmailSender, ProtonEmailSender>();
+            services.AddScoped<IEmailSenderFactory, EmailSenderFactory>();
+        });
+    }
+
+    public static void AddDbContext(this WebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                var dbPath = Environment.GetEnvironmentVariable("DB__PATH");
+                options.UseSqlite(dbPath);
+            });
+
+            services.AddScoped<IDbContext>(provider => provider.GetRequiredService<AppDbContext>());
+        });
+    }
+}
